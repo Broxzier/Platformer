1bb58bf5928067531d938582eaf5e53d12ca97ae
Hielke Morsink <hielke.morsink@gmail.com>
2015-11-22 12:57:21 +0100

    Displaying function name, and consistent formatting.

--- Platformer/src/platformer.cpp
+++ Platformer/src/platformer.cpp
@@ -3,22 +3,35 @@
 #include <cstdio>
 #include <cstring>
 
+bool g_LogVerbose = true;
+
 #define FILE (strrchr(__FILE__, '\\') ? strrchr(__FILE__, '\\') + 1 : __FILE__)
-#define DebugWarning(message) std::printf("%s(%i): " message "\n", FILE, __LINE__)
-#define DebugLog(message) std::puts(message)
+#define LogError(message)	if (g_LogVerbose) std::printf("  Error: %s:%i (%s): " message "\n", FILE, __LINE__, __FUNCTION__)
+#define LogWarning(message) if (g_LogVerbose) std::printf("  Warning: %s:%i (%s): " message "\n", FILE, __LINE__, __FUNCTION__)
+#define LogMessage(message) if (g_LogVerbose) std::printf("  %s:%i (%s): " message "\n", FILE, __LINE__, __FUNCTION__)
+#define LogText(message)	std::printf("    "message"\n")
 
 Platformer::Platformer() {}
 Platformer::~Platformer() {}
 
-void Platformer::Start() {
-	DebugLog("Starting up the platformer");
+void Platformer::Start()
+{
+	LogText("");
+	LogMessage("");
+
 }
 
-void Platformer::Run() {
-	DebugLog("Running");
+void Platformer::Run()
+{
+	LogMessage("");
+	for (size_t i = 0; i < 10; i++)
+	{
+		LogText("Running");
+	}
 }
 
-void Platformer::Close() {
-	DebugLog("Closing platformer");
+void Platformer::Close()
+{
+	LogMessage("");
 	std::getchar();
 }
